237f30775b609528bd8cdb48a9e49770b3cc9773
Carlos <carlos_seijo_r@hotmail.com>
2018-09-04 10:27:12 +0200

    Update jquery.slides.js

--- js/jquery.slides.js
+++ js/jquery.slides.js
@@ -5,8 +5,8 @@
     var Plugin, defaults, pluginName;
     pluginName = "slidesjs";
     defaults = {
-      width: 1000,
-      height: 200,
+      width: screen.width,
+      height: 500,
       start: 1,
       navigation: {
         active: true,
